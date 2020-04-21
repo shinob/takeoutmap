b39f25c58d3811ae1f01e98ecbd4b96cf0a89b97
Shinobu Kimura <shinob@mbb.nifty.ne.jp>
2020-04-21 16:07:43 +0900

    new file

---
+++ src/points_yonago.js
@@ -0,0 +1,51 @@
+//中心座標の初期値
+var lat = 35.4281791;
+var lng = 133.331021;
+
+function load_points() {
+    
+    return [
+/*
+        [[],	"", ""],
+*/
+        [[35.4495782, 133.3152237],	"あぷりこ", "https://www.instagram.com/aplico_2017/"],
+        [[35.424418, 133.333667],	"日々の糧", "https://www.instagram.com/hibinokateyonago/"],
+        [[35.4301703, 133.3311164],	"カフェ ザ パーク 米子", "https://www.instagram.com/cafethepark_yonago/"],
+        [[35.4294141, 133.3322668],	"ドドド", "https://www.instagram.com/dododo1952/"],
+        [[35.424272, 133.335],	"焼肉韓国創作料理店カムサ 米子本店", "https://www.instagram.com/yonago_kamusa/"],
+        
+        [[35.4343998, 133.3307297],	"ZIO食堂", "https://www.instagram.com/zio_syokudou/"],
+        [[35.431164, 133.333354],	"MOON & SPOON", "https://www.instagram.com/moon_and_spoon/"],
+        [[35.442101, 133.336252],	"ノンカフェ", "https://www.instagram.com/non_cafe/"],
+        [[35.4559942, 133.3375774],	"焼肉韓食房だんだん", "https://www.instagram.com/dangdang_yonago/"],
+        [[35.4511584, 133.3591605],	"キッチンノスタルジヤ", "https://www.instagram.com/kitchen_nostalgiya/"],
+        [[35.423911, 133.334695],	"くいもの屋わん 米子駅前店", "https://www.instagram.com/wan_yonago/"],
+        [[35.4328714, 133.3309076],	"焼肉一八", "https://www.instagram.com/yakiniku_18/"],
+        [[35.4262052, 133.3424053],	"焼肉ぷるこぎ米子店", "https://www.instagram.com/p/B-7CllbgoOh/"],
+        [[35.423671, 133.335146],	"カゼトソラ", "https://www.instagram.com/a_littlekitchen_kazetosora/"],
+        [[35.4351544, 133.3319704],	"SO-JU ソウジュ", "https://www.facebook.com/SO-JU-1648844875356549/"],
+        
+        [[35.4254832, 133.3324742],	"イタリアンバール・ピッコロ", "https://www.instagram.com/italianbarpiccolo/"],
+        [[35.4324171, 133.3275931],	"ながた茶店 岩倉町本店", "https://www.facebook.com/有限会社-長田茶店-815412565159557/"],
+        [[35.4528287, 133.3322008],	"籠乃鶏大山 三柳店", "https://www.instagram.com/kagonotori_mituyanagi/"],
+        [[35.424588, 133.334279],	"和食居酒屋 旬門 米子駅前本店", "https://www.instagram.com/shunmon.yonago/"],
+        [[35.4537585, 133.3435516],	"八仙閣", "https://yonago.mypl.net/shop/00000354328/news?d=1771407"],
+        [[35.4316408, 133.3342671],	"魚ろばた 海座 米子店", "https://www.instagram.com/p/B-1IwprnClG/"],
+        [[35.4216977, 133.3332284],	"レストラン ル・ポルト", "https://leport.org/takeout/"],
+        [[35.4396388, 133.4396906],	"真名井せせらぎレストラン", "https://www.instagram.com/p/B-vPtEqB4hi/"],
+        [[35.4256263, 133.328468],	"ANA Crowne Plaza Yonago", "https://www.instagram.com/p/B-vhrJjAU10/"],
+        [[35.4517519, 133.3303902],	"さわ食堂", "https://www.instagram.com/p/B-tZHIhB_Ko/"],
+        
+        [[35.4479044, 133.3528071],	"おむすび三休", "https://yonago.mypl.net/shop/00000360547/"],
+        [[35.4330056, 133.3314669],	"トゥクトゥク", "https://www.instagram.com/tuk223334/"],
+        [[35.440139, 133.326701],	"こめ蔵", "https://www.facebook.com/komekura.donburi/"],
+        [[35.4313623, 133.3326911],	"麺処 想", "https://goo.gl/maps/x4LXLEpj5cfS9K638"],
+        [[35.431446, 133.3350084],	"カリフォルニアキッチン米子", "https://www.instagram.com/p/B-gHGinnHlH/"],
+        [[35.4290382, 133.3319164],	"マカロニ食堂", "https://www.instagram.com/p/B-loXTEnFfH/"],
+        [[35.4291045, 133.3318245],	"ランドアンドイヤーズ", "https://www.instagram.com/p/B-lRVNpny6u/"],
+        [[35.4240518, 133.3344847],	"美酒佳肴ゆらく", "https://www.instagram.com/p/B-Lum_iHdvq/"],
+        [[35.4329927, 133.3323925],	"桔梗屋", "https://www.instagram.com/p/B-flR67HAML/"],
+        [[35.4418618, 133.305296],	"オーンズ", "https://www.instagram.com/p/B-intkZn2My/"]
+    ];
+    
+}
