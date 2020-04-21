988b1bcf0d0dd3bdb190ce4818d864e17f354fd4
Shinobu Kimura <shinob@mbb.nifty.ne.jp>
2020-04-21 09:36:16 +0900

    new file

---
+++ src/points.js
@@ -0,0 +1,12 @@
+//中心座標の初期値
+var lat = 35.468606;
+var lng = 133.059026;
+
+function load_points() {
+    
+    return [
+        [[35.4658345, 133.0599149],	"[Name of shop]","https://www.google.com"],
+        [[35.373385, 132.7359618],	"[Name of Shop]", "https://www.microsoft.com"]
+    ];
+    
+}
