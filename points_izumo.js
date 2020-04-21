f491f41b6ef0c57e0074d148ff9956b988cee9c9
Shinobu Kimura <shinob@mbb.nifty.ne.jp>
2020-04-21 09:59:01 +0900

    new file

---
+++ points_izumo.js
@@ -0,0 +1,35 @@
+//中心座標の初期値
+var lat = 35.360870;
+var lng = 132.756664;
+
+function load_points() {
+    
+    return [
+        [[35.362202, 132.7430118],	"トロントセット", "https://www.facebook.com/トロントセット-1916000211978407/"],
+        [[35.399901, 132.6831172],	"山太", "https://www.facebook.com/山太santa-230036527187629/"],
+        
+        [[35.3630458, 132.7656604],	"ホテル武志山荘", "https://www.takeshi-sanso.co.jp/"],
+        [[35.3977135, 132.6794232],	"きんぐ", "https://www.facebook.com/きんぐ-159327860744158/"],
+        [[35.3620579, 132.7566728],	"居酒屋 BAMBOO", "https://www.facebook.com/居酒屋-ＢＡＭＢＯＯ-660529100759329/"],
+        [[35.3647523, 132.7556406],	"凡蔵", "https://www.facebook.com/居酒屋-凡蔵-1465046390404129/"],
+        [[35.3663816, 132.7559113],	"おうちごはんカフェ 豆花", "https://www.facebook.com/cafemamehana/"],
+        [[35.3612977, 132.7595823],	"ラーメン篠寛", "https://shinokan.com"],
+        [[35.3704183, 132.7650885],	"鉄板お好み焼Syu", "https://izumo.mypl.net/shop/00000344623/"],
+        [[35.435887, 132.8173635],	"鍛冶屋と料理", "https://www.facebook.com/pages/鍛冶屋と料理/987176084694935"],
+        [[35.3349569, 132.6867659],	"相撲食堂ちゃんこちゃん", "https://goo.gl/maps/JwNhEKkbDSqxEaEo8"],
+        [[35.4383063, 132.8211686],	"ごはん屋 棉の花", "http://momen-kaidou.jp/news/?p=686"],
+        [[35.3663893, 132.7414433],	"レガーレ(REGARE)", "https://goo.gl/maps/r49EUuQnM63oKMtz9"],
+        
+        [[35.3735538, 132.7448877],	"ランコントレ(Rencontrer)", "https://www.facebook.com/出雲-ランコントレフランス料理店-610981219025271/"],
+        [[35.3608505, 132.7539933],	"Taste-Brooklyn Dining", "https://www.instagram.com/tastebrooklyn/"],
+        [[35.364192, 132.7561504],	"THE STRUCTURE", "https://www.instagram.com/the.structure/"],
+        [[35.4066898, 132.8351442],	"プチレストラン クルトン", "https://www.facebook.com/culuton/"],
+        [[35.373898, 132.764407],	"お食事処つむぎ", "https://tabelog.com/shimane/A3202/A320201/32006010/"],
+        [[35.369301, 132.766433],	"山さ紀とんとん亭", "https://www.facebook.com/tontontei/"],
+        [[35.357760, 132.729896],	"とんかつ専門店 薩摩居酒屋 かのや", "https://www.facebook.com/tonkatsukanoya/"],
+        [[35.373341, 132.758858],	"Cafe LEON", "https://www.facebook.com/Cafe-LEON-110829803796110/"],
+        [[35.372373, 132.759183],	"restaurant CINQ SENS", "https://www.facebook.com/cinq.sens.izumo/"],
+        [[35.373385, 132.7359618],	"出雲翠苑", "https://www.facebook.com/izumosuien"]
+    ];
+    
+}
