77fbab0cadaeadc30769a4d4d93b006df3991582
Shinobu Kimura <shinob@mbb.nifty.ne.jp>
2020-04-22 15:20:50 +0900

    add init_zoom parameter.

--- src/points.js
+++ src/points.js
@@ -2,9 +2,14 @@
 var lat = 35.468606;
 var lng = 133.059026;
 
+var init_zoom = 14;
+
 function load_points() {
     
     return [
+/*
+        [[],	"", ""],
+*/
         [[35.4658345, 133.0599149],	"[Name of shop]","https://www.google.com"],
         [[35.373385, 132.7359618],	"[Name of Shop]", "https://www.microsoft.com"]
     ];
