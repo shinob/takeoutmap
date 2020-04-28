618dbb2cb7295d0abc6cebd88bd1cb04598b1b36
Shinobu Kimura <shinob@mbb.nifty.ne.jp>
2020-04-28 17:18:11 +0900

    Update takeout_base.js

--- src/takeout_base.js
+++ src/takeout_base.js
@@ -4,7 +4,7 @@ var newmap = L.tileLayer('https://cyberjapandata.gsi.go.jp/xyz/seamlessphoto/{z}
     minZoom: 12
 });
 
-var oldmap = L.tileLayer("http://map.blueomega.jp/osm_tiles/{z}/{x}/{y}.png", {
+var oldmap = L.tileLayer("https://map.blueomega.jp/osm_tiles/{z}/{x}/{y}.png", {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
     maxZoom: 18,
     minZoom: 12
@@ -19,6 +19,12 @@ var list = "";
 var m = [];
 
 var init_zoom = 14;
+
+var initLatlng = {
+	matsue : [35.468606, 133.059026],
+	izumo : [35.360870, 132.756664], 
+	yonago : [35.4281791, 133.331021],
+};
 
 var mymap;
     
@@ -69,12 +75,37 @@ function add_points() {
         }).addTo(mymap);
     }
     
+    frm_select  = "<form name='shopSelector'><b>店舗選択</b><br /><select name='shop' onchange='moveToSelected()' style='width: 100%;'>";
+    frm_select += "<option value=-1>店舗を選択して下さい。</option>"
+    
     for (i=0; i<points.length; i++) {
+        
+        dsc  = "<div style='text-align: center;'>";
+        dsc += "<a href='" + points[i][2] + "' target='_blank'>" + points[i][1] + "</a>";
+        
+        item = "<div style='float: left; text-align: center;' onclick='moveTo([" + points[i][0] + "], " + i + ")'>";
+        item += points[i][1];
+        
+        frm_select += "<option value=" + i + ">" + points[i][1] + "</option>";
+        
+        if (points[i].length > 3) {
+	        dsc += "<br /><br /><a href='tel:" + points[i][3] + "'>" + points[i][3] + "</a>";
+	        //item += "<br />" + points[i][3];
+        } else {
+	        
+        }
+        
+        dsc += "</div>";
+        item += "</div>";
+        
         m.push(L.marker(points[i][0]).addTo(mymap)
-            .bindPopup("<a href='" + points[i][2] + "' target='_blank'>" + points[i][1] + "</a>")); //.openPopup());
-        list += "<div style='float: left;' onclick='moveTo([" + points[i][0] + "], " + i + ")'>" + points[i][1] + "</div>";
+            .bindPopup(dsc)); //.openPopup());
+        list += item;
     }
     
+    frm_select += "</select></form>";
+    
+    document.getElementById("frm_select").innerHTML = frm_select;
     document.getElementById("list").innerHTML = list;
     document.getElementById("info").innerHTML = "登録店舗数 " + points.length + "店";
 
@@ -86,4 +117,14 @@ function moveTo(latlng, i) {
     m[i].openPopup();
 }
 
+function moveToSelected() {
+	i = document.shopSelector.shop.selectedIndex - 1;
+	//alert(i);
+	moveTo(points[i][0], i);
+}
+
+function setCenter(latlng) {
+    mymap.setView(latlng, init_zoom);
+    m.setLatLng(latlng);
+}
 
